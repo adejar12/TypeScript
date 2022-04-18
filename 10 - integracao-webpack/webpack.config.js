bd22aa2efe0d09caeb9367ff93713097729a5d8d
adejar12 <adejarjunior18@gmail.com>
2022-04-18 19:55:55 -0300

    chore: configurando webpack

---
+++ 10 - integracao-webpack/webpack.config.js
@@ -0,0 +1,24 @@
+const { resolve } = require("path");
+const path = require("path");
+
+module.exports = {
+  module: "production", // pode ser 2 tipos: development ou production
+  entry: "./src/main.js", // a partir de qual arquivo o webpack vai começar a buscar os arquivos
+  output: {
+    // na hora de exportar os arquivos
+    filename: "app.min.js", // o nome do arquivo que será gerado
+    path: path.join(__dirname, "dist"), // a localização do arquivo
+  },
+  resolve: {
+    extensions: [".ts", ".js"], // extensões que o webpack vai procurar
+  },
+  module: {
+    rules: [
+      {
+        teste: /\.ts$/, // qualquer arquivo que tenha a extensão js serão processados por essa regra
+        use: "ts-loader", // qual loader vai ser usado, que é responsavel por ler o arquivo ts e transformar em js
+        exclude: /node_modules/, // não vai ser processado os arquivos que estão na pasta node_modules
+      },
+    ],
+  },
+};
